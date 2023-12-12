27efa5091f1ad47653147eaa030b797213ec09c1
mmanultra <mmanultra2@gmail.com>
2023-12-12 11:55:56 -0500

    update .gitignore

--- RevPay/src/main/java/com/revature/RevPay/RevPayApplication.java
+++ RevPay/src/main/java/com/revature/RevPay/RevPayApplication.java
@@ -9,6 +9,7 @@ public class RevPayApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext iocContainer = SpringApplication.run(RevPayApplication.class, args);
+		System.out.println("");
 	}
 
 }
